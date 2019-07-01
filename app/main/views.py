4a2f64ff33b0187e1e04ba2eedfaad2060958d36
Antony <amwaniki180@gmail.com>
2019-07-01 09:44:30 +0300

    404 error page

--- app/main/views.py
+++ app/main/views.py
@@ -1,2 +1,15 @@
 from flask import render_template
 from app import app
+
+
+
+
+
+@main.route('/')
+def index():
+    '''
+    view root page function that returns the index page and its data
+    '''
+    pitches = Pitch.query.all()
+    title = "Home - welcome to pitches...Best pitch app"
+    return render_template("index.html", pitches = pitches,title = title)
