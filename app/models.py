fc0a8eca8b72261ff54e5327727ab4dc62c0ce62
Antony <amwaniki180@gmail.com>
2019-07-02 07:38:37 +0300

    comment.html

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
-    user_id = db.Column(db.Foreignkey('user.id'))
+    
 
     def __repr__(self):
         return f'User {self.username}'
