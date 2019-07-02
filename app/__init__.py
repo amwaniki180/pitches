1172a243d5fdda6df37273733b18397558ea0f24
Antony <amwaniki180@gmail.com>
2019-07-02 10:25:06 +0300

    login_manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchem
+from flask_login import LoginManager
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
