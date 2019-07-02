7a17f5345a36245543e5b349fc21b257f3350929
Antony <amwaniki180@gmail.com>
2019-07-02 11:13:06 +0300

    user mixin

--- app/models.py
+++ app/models.py
@@ -1,15 +1,16 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 
-
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
-    pass_secure = db.Column(db.String(255))
 
-    def __repr__(self):
-        return f'User {self.username}'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(255))
+    
 class Pitch(db.Model):
     """
     This is the class which we will use to create the pitches for the app
